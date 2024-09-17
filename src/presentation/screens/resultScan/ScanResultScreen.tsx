d9fb55ae05be481c2b20bf647fc79f045b0ae055
Mauri <mauri.monzon91@gmail.com>
2024-09-17 11:11:06 -0300

    Poniendo la informacion en inputs

--- src/presentation/screens/resultScan/ScanResultScreen.tsx
+++ src/presentation/screens/resultScan/ScanResultScreen.tsx
@@ -1,18 +1,59 @@
 import {StackScreenProps} from '@react-navigation/stack';
-import React from 'react';
-import {Text, View} from 'react-native';
+import React, {useState} from 'react';
+import {Alert, StyleSheet, Text, View} from 'react-native';
 import {RootStackParams} from '../../navigation/StackNavigator';
+import {Button, Input} from '@ui-kitten/components';
 
 interface Props extends StackScreenProps<RootStackParams, 'ScanResultScreen'> {}
 
-export const ScanResultScreen = ({route}:Props) => {
-  const info = route.params.scannedData
-console.log(info);
+export const ScanResultScreen = ({route, navigation}: Props) => {
+  const {scannedData} = route.params;
+  const [formData, setFormData] = useState(scannedData.split('@'));
 
-  
+  const handleChange = (index: number, value: string) => {
+    const updatedData = [...formData];
+    updatedData[index] = value;
+    setFormData(updatedData);
+  };
+
+  const handleSubmit = () => {
+    // Aquí podrías validar y enviar los datos o guardarlos en el estado
+    const formattedData = formData.join('@'); // Reconstruye la cadena de datos
+    // Aquí podrías enviar los datos a un servidor o guardarlos en el estado
+    Alert.alert(
+      'Datos Guardados',
+      `Los datos se han guardado: ${formattedData}`,
+    );
+    // Navegar a la siguiente pantalla o realizar otra acción
+  };
+
   return (
-    <View>
-      <Text>ScanResultScreen</Text>
+    <View style={styles.container}>
+      {formData.map((data, index) => (
+        <Input
+          key={index}
+          placeholder={`Campo ${index + 1}`}
+          value={data}
+          onChangeText={text => handleChange(index, text)}
+          style={styles.input}
+        />
+      ))}
+      <Button status="primary" onPress={handleSubmit}>
+        Guardar
+      </Button>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 20,
+  },
+  input: {
+    marginBottom: 10,
+    width: '100%',
+  },
+});
